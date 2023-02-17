a5392ac2b1e663faf86f505c5d55f43157af9a4b
Johannes Tuikkala <johannes@vaadin.com>
2023-02-17 13:07:46 +0200

    use push and remove web.xml in favor of using @WebServlet params, updated Vaadin versions

--- src/main/java/org/vaadin/mprdemo/LegacyView.java
+++ src/main/java/org/vaadin/mprdemo/LegacyView.java
@@ -3,12 +3,18 @@ package org.vaadin.mprdemo;
 import com.vaadin.ui.*;
 
 public class LegacyView extends VerticalLayout {
-	
+
+	private final Label label;
+
 	public LegacyView() {
-		addComponent(new Label("Here we are in V7!"));
+		label = new Label("Here we are in V7!");
+		addComponent(label);
 		addComponent(new Button("Show notification", e-> {
 			Notification.show("V7 notification");
 		}));
 	}
 
+    public void setValue(int counter) {
+		label.setValue("Backgroud update (V7) "+counter);
+    }
 }
--- src/main/java/org/vaadin/mprdemo/MyUI.java
+++ src/main/java/org/vaadin/mprdemo/MyUI.java
@@ -1,49 +1,85 @@
 package org.vaadin.mprdemo;
 
-import com.vaadin.annotations.Push;
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.router.RouterLink;
-
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.mpr.LegacyWrapper;
 import com.vaadin.mpr.core.LegacyUI;
 import com.vaadin.mpr.core.MprTheme;
 import com.vaadin.mpr.core.MprWidgetset;
-import com.vaadin.server.VaadinSession;
-import com.vaadin.shared.communication.PushMode;
-import com.vaadin.shared.ui.ui.Transport;
 
-@Push(value = PushMode.AUTOMATIC, transport = Transport.WEBSOCKET)
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@Push(value = PushMode.AUTOMATIC, transport = Transport.WEBSOCKET_XHR)
 @Route("")
 @MprTheme("mytheme")
 @CssImport("custom.css")
 @LegacyUI(OldUI.class)
 @MprWidgetset("org.vaadin.mprdemo.MyWidgetSet")
-public class MyUI extends VerticalLayout implements RouterLayout {
+public class MyUI extends VerticalLayout {
+	private final LegacyView legacyComponent;
+	private final VerticalLayout pushLayout = new VerticalLayout();
+
+	@WebServlet(value = {"/ui/*", "/VAADIN"}, asyncSupported = true, initParams = {
+			@WebInitParam(name = "closeIdleSessions", value = "false"),
+			@WebInitParam(name = "org.atmosphere.container.JSR356AsyncSupport.mappingPath", value = "/ui"),
+			@WebInitParam(name = "timeout", value = "-1")
+	})
+	public static class Servlet extends VaadinServlet {
+	}
 
 	@SuppressWarnings("deprecation")
 	public MyUI() {
-		add(new LegacyWrapper(new LegacyView()));
-
+		H3 flowLabel = new H3("Flow H3");
+		add(pushLayout);
+		add(flowLabel);
+		legacyComponent = new LegacyView();
+		add(new LegacyWrapper(legacyComponent));
 	}
 
 	@Override
 	public void onAttach(AttachEvent event) {
-		if (event.getSession().getService().getDeploymentConfiguration().isProductionMode()) {
-			event.getUI().getPage().executeJs("window.vaadin.debug=false;");
-		}
+		FeederThread thread = new FeederThread(event.getUI(), this, legacyComponent);
+		thread.start();
 	}
 
+	private static class FeederThread extends Thread {
+		private final UI ui;
+		private final VerticalLayout view;
+		private LegacyView legacyView;
+		private int count = 0;
+
+		public FeederThread(UI ui, VerticalLayout view, LegacyView legacyView) {
+			this.ui = ui;
+			this.view = view;
+			this.legacyView = legacyView;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (count < 10) {
+					Thread.sleep(2000);
+					String message = "This is update " + count++;
+					ui.access(() -> view.add(new Span(message)));
+					ui.access(() -> legacyView.setValue(count));
+					System.out.println(message);
+				}
+				ui.access(() -> {
+					view.add(new Span("Done updating"));
+				});
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
